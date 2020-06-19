7011c5f6e0e161378eb8e53ba2b592a6a195c01e
Yugi10 <67090447+Yugi10@users.noreply.github.com>
2020-06-19 09:46:32 +0530

    Update PositiveOfListRange.py

--- PositiveOfListRange.py
+++ PositiveOfListRange.py
@@ -18,3 +18,27 @@ for a in range (0,n) :
         
 print ("\nYour list with only positive elements is :\n" ,output)
 
+How many elements do you want in your list ?
+ 5
+
+Please enter the element number 1 of the list :
+1
+
+Please enter the element number 2 of the list :
+-2
+
+Please enter the element number 3 of the list :
+3
+
+Please enter the element number 4 of the list :
+-4
+
+Please enter the element number 5 of the list :
+5
+
+Your list is :
+ [1, -2, 3, -4, 5]
+
+Your list with only positive elements is :
+ [1, 3, 5]
+
